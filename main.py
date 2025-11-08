75c31d1970616a3d2cea12803714d7398e522215
Yurii Bodnar <yurii.bodnar@globallogic.com>
2025-11-08 23:06:37 +0200

    feat(python): add 6 business questions for accidents dataset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from extract import load_dataframes
+from transform.accidents import transform_accidents_df 
 
 if __name__ == "__main__":
     # SparkSession initialization
@@ -9,10 +10,17 @@ if __name__ == "__main__":
     base_path = "/app/Data"
 
     # Loading all CSVs into a DataFrame
+    print("=== STAGE 1: EXTRACT ===")
     dfs = load_dataframes(spark, base_path)
 
-    print("\n📊 Rows number in each dataset:")
+    print("\nRows number in each dataset:")
     for name, df in dfs.items():
         print(f"{name}: {df.count()}")
 
+    print("\n=== STAGE 2: TRANSFORM (Business-questions) ===")
+    
+    transform_accidents_df(dfs)
+    # TODO: insert transform stage for other datasets
+
+    print("\n=== Завершення роботи =====")
     spark.stop()
