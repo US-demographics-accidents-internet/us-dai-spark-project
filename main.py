361dafd8cb85212528cf9268e172834fb8f299c4
Andrii Maherovskyi <andrik0404@gmail.com>
2025-11-10 11:03:09 +0200

    remove unnecessary commented code from main

--- main.py
+++ main.py
@@ -21,30 +21,7 @@ if __name__ == "__main__":
     for name, df in dfs.items():
         print(f"{name}: {df.count()}")
 
-    # # Merge 2 DataFrames (pusa, pusb) into demographic_df
-    # demographic_df = merge_equal_dataframes(dfs["pusa"], dfs["pusb"])
-    # dfs["demographic_df"] = demographic_df
-
-    
-    # print("\n=== STAGE 2: TRANSFORM (Business-questions) ===")
-
-    # internet_demographic_transformation = InternetDemographicTransformation() 
-
-    # results = internet_demographic_transformation.invoke_pipeline(spark=spark,
-    #                                                               demographics_df=dfs["demographic_df"], 
-    #                                                               internet_df=dfs["internet"])
-
-    # transform_accidents_df(dfs)
-
-    # transform_demographics_df(dfs, spark)
-
-    # accidents_demographics_transformation = AccidentsDemographicsTransformation() 
-
-    # accidents_demographics_transformation.invoke_pipeline(spark=spark,
-    #                                                               df_pusa=dfs["pusa"],
-    #                                                               df_pusb=dfs["pusb"], 
-    #                                                               df_accidents=dfs["accidents"])
-
+    # Transformational stage
     transform_stage(dfs, spark)
 
     print("\n=== Завершення роботи =====")
